ae8652ce8f08a0a364e378550a17da0337a2320d
Cole Freeman <cole.freeman@coyote.com>
2021-03-08 22:00:38 -0600

    Added merge class and functions

--- merge/merge.py
+++ merge/merge.py
@@ -1,2 +1,51 @@
-from groupby import GroupBy
+import numpy as np
 
+from groupby import GroupBy
+from .merge_functions import _inner_merge_jit, _inner_merge
+
+class Merge:
+    def __init__(self, l, r, l_on, r_on):
+        """
+            The Merge object implements different types of merges between two arrays. It utilizes
+            the sorting an group-finding characterstics of the GroupBy object to facilitate the
+            merges, as well as functions from NumPy's setops suite.
+        """
+        
+        self.l_on = l_on
+        self.r_on = r_on
+        
+        # Merge supports l and r parameters to be either arrays, or GroupBy objects. 
+        # If they are arrays, we need to perform GroupBy initialization to get keys
+        if isinstance(l, GroupBy) and (l.by == l_on):
+            self.l_gb = l
+        else:
+            self.l_gb = GroupBy(l, l_on)
+            
+        if isinstance(r, GroupBy) and (r.by == r_on):
+            self.r_gb = r
+        else:
+            self.r_gb = GroupBy(r, r_on)
+        
+    
+    def inner(self, jitted=True):
+        """ inner join on the specified columns of the Merge object """
+        
+        # We'll need contiguous arrays to get the proper view of our keys
+        l_keyc, r_keyc = np.ascontiguousarray(self.l_gb.keys), np.ascontiguousarray(self.r_gb.keys)
+        dtype = [(f'{i}', l_keyc.dtype) for i in range(l_keyc.shape[1])] # l_gb.on and r_gb.on should be of same length
+        # Get a view of the keys (the one stored in the GroupBy objects may have differently named fields)
+        l_keyv, r_keyv = l_keyc.view(dtype)[:, 0], r_keyc.view(dtype)[:, 0]
+        
+        # Find the intersection between the two key-sets and the indices in each set for those intersections
+        intersect, l_idx, r_idx = np.intersect1d(l_keyv, r_keyv, 
+                                                 assume_unique=True, return_indices=True)
+
+        if jitted:
+            res = _inner_merge_jit(idx=self.l_gb.idx, l_vals=self.l_gb.vals, r_vals=self.r_gb.vals, 
+                                    n_intersect=intersect.shape[0], l_gr_idx=self.l_gb.gr_idx,
+                                    r_gr_idx=self.r_gb.gr_idx, l_idx=l_idx, r_idx=r_idx)
+        else:
+            res = _inner_merge(idx=self.l_gb.idx, l_vals=self.l_gb.vals, r_vals=self.r_gb.vals, 
+                               n_intersect=intersect.shape[0], l_gr_idx=self.l_gb.gr_idx,
+                               r_gr_idx=self.r_gb.gr_idx, l_idx=l_idx, r_idx=r_idx)
+        return res
---
+++ merge/merge_functions.py
@@ -0,0 +1,80 @@
+import numpy as np
+from numba import jit
+
+@jit
+def _inner_merge_jit(idx, l_vals, r_vals, n_intersect, 
+                      l_gr_idx, r_gr_idx, l_idx, r_idx):
+    """ Helper jitted function for inner merge """
+    # Make the results object of the proper proportions
+    l_counts = np.diff(l_gr_idx)[l_idx] # get counts per group
+    r_counts = np.diff(r_gr_idx)[r_idx]
+
+    group_n = l_counts * r_counts
+    res_group_i = np.concatenate((np.array([0]), np.cumsum(group_n)))
+
+    n = res_group_i[-1] # number of total rows is the dot product of the each merge-group's lengths
+    m = idx.shape[1] + l_vals.shape[1] + r_vals.shape[1]
+
+    # Make an empty array to be filled in piece by piece
+    res = np.empty((n,m), dtype=idx.dtype)
+
+    idx_end = idx.shape[1]
+    l_end = idx_end + l_vals.shape[1]
+
+    for i in range(n_intersect):
+
+        index = idx[l_gr_idx[l_idx[i]]] # a single row representation of the index columns (for left position)
+
+        l_arr = l_vals[l_gr_idx[l_idx[i]]:l_gr_idx[l_idx[i]+1]]
+        r_arr = r_vals[r_gr_idx[r_idx[i]]:r_gr_idx[r_idx[i]+1]] 
+
+        res_gr = res[res_group_i[i]:res_group_i[i+1]]
+
+        res_gr[:, :idx_end] = index
+
+        l_group_i = np.arange(l_counts[i]+1) * r_counts[i] # positions of each new row of l_arr in res_gr
+
+        for j in range(l_counts[i]):
+            res_gr[l_group_i[j]:l_group_i[j+1], idx_end:l_end] = l_arr[j]
+            res_gr[l_group_i[j]:l_group_i[j+1], l_end:] = r_arr
+
+    return res
+
+
+def _inner_merge(idx, l_vals, r_vals, n_intersect, 
+                      l_gr_idx, r_gr_idx, l_idx, r_idx):
+    """ Helper function for inner merge """
+    # Make the results object of the proper proportions
+    l_counts = np.diff(l_gr_idx)[l_idx] # get counts per group
+    r_counts = np.diff(r_gr_idx)[r_idx]
+
+    group_n = l_counts * r_counts
+    res_group_i = np.concatenate((np.array([0]), np.cumsum(group_n)))
+
+    n = res_group_i[-1] # number of total rows is the dot product of the each merge-group's lengths
+    m = idx.shape[1] + l_vals.shape[1] + r_vals.shape[1]
+
+    # Make an empty array to be filled in piece by piece
+    res = np.empty((n,m), dtype=idx.dtype)
+
+    idx_end = idx.shape[1]
+    l_end = idx_end + l_vals.shape[1]
+
+    for i in range(n_intersect):
+
+        index = idx[l_gr_idx[l_idx[i]]] # a single row representation of the index columns (for left position)
+
+        l_arr = l_vals[l_gr_idx[l_idx[i]]:l_gr_idx[l_idx[i]+1]]
+        r_arr = r_vals[r_gr_idx[r_idx[i]]:r_gr_idx[r_idx[i]+1]] 
+
+        res_gr = res[res_group_i[i]:res_group_i[i+1]]
+
+        res_gr[:, :idx_end] = index
+
+        l_group_i = np.arange(l_counts[i]+1) * r_counts[i] # positions of each new row of l_arr in res_gr
+
+        for j in range(l_counts[i]):
+            res_gr[l_group_i[j]:l_group_i[j+1], idx_end:l_end] = l_arr[j]
+            res_gr[l_group_i[j]:l_group_i[j+1], l_end:] = r_arr
+
+    return res
