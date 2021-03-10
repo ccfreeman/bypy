44d8312001ca3e4b36a649389e8738cd7f95a340
ccfreeman <cole.freeman9@gmail.com>
2021-03-09 22:10:39 -0600

    Added left/right merge

--- merge/merge.py
+++ merge/merge.py
@@ -1,7 +1,8 @@
 import numpy as np
 
 from groupby import GroupBy
-from .merge_functions import _inner_merge_jit, _inner_merge
+from .merge_functions import _inner_merge_jit, _inner_merge, _lr_merge_constr_res
+
 
 class Merge:
     def __init__(self, l, r, l_on, r_on):
@@ -24,20 +25,31 @@ class Merge:
             self.r_gb = r
         else:
             self.r_gb = GroupBy(r, r_on)
-        
-    
-    def inner(self, jitted=True):
-        """ inner join on the specified columns of the Merge object """
-        
+
+        if isinstance(self.l_gb.idx.dtype, np.floating) and isinstance(self.r_gb.idx.dtype, np.floating):
+            self.null_val = np.nan
+        else:
+            self.null_val = -99999
+      
+      
+    def _merge_prefix(self, l_gb_keys, r_gb_keys):
+        """ A shared function between several merge types """
         # We'll need contiguous arrays to get the proper view of our keys
-        l_keyc, r_keyc = np.ascontiguousarray(self.l_gb.keys), np.ascontiguousarray(self.r_gb.keys)
+        l_keyc, r_keyc = np.ascontiguousarray(l_gb_keys), np.ascontiguousarray(r_gb_keys)
         dtype = [(f'{i}', l_keyc.dtype) for i in range(l_keyc.shape[1])] # l_gb.on and r_gb.on should be of same length
         # Get a view of the keys (the one stored in the GroupBy objects may have differently named fields)
         l_keyv, r_keyv = l_keyc.view(dtype)[:, 0], r_keyc.view(dtype)[:, 0]
         
         # Find the intersection between the two key-sets and the indices in each set for those intersections
-        intersect, l_idx, r_idx = np.intersect1d(l_keyv, r_keyv, 
-                                                 assume_unique=True, return_indices=True)
+        intersect, l_idx, r_idx = np.intersect1d(l_keyv, r_keyv, assume_unique=True, 
+                                                 return_indices=True)
+        return intersect, l_idx, r_idx
+    
+    
+    def inner(self, jitted=True):
+        """ inner join on the specified columns of the Merge object """
+        
+        intersect, l_idx, r_idx = self._merge_prefix(self.l_gb.keys, self.r_gb.keys)
 
         # The creation of the return array is spedup with numba no-python mode
         if jitted:
@@ -49,3 +61,39 @@ class Merge:
                                n_intersect=intersect.shape[0], l_gr_idx=self.l_gb.gr_idx,
                                r_gr_idx=self.r_gb.gr_idx, l_idx=l_idx, r_idx=r_idx)
         return res
+    
+    
+    def left(self, null_val=None):
+        """ left outer join on specified columns """
+        if null_val is None:
+            null_val = self.null_val
+        return self._lr_merge(l_gb=self.l_gb, r_gb=self.r_gb, how='left', null_val=null_val)
+    
+    
+    def right(self, null_val=None):
+        """ right outer join on specified columns """
+        if null_val is None:
+            null_val = self.null_val
+        return self._lr_merge(l_gb=self.r_gb, r_gb=self.l_gb, how='right', null_val=null_val)
+    
+    
+    def _lr_merge(self, l_gb, r_gb, how, null_val):
+        """
+            As left and right outer joins are symmetrical, we can reuse code, and just appropriately set the 
+            left and right data objects to return the desired result. The default is left merge.
+            
+            The numpy null value is a floating point, so we need to designate what will signify null values 
+            for other datatypes, such as integers. Default is -99,999.
+        """
+        if how == 'left':
+            how = 0
+        elif how == 'right':
+            how = 1
+            
+        intersect, l_idx, r_idx = self._merge_prefix(l_gb.keys, r_gb.keys)
+        
+        return _lr_merge_constr_res(idx=l_gb.idx, l_vals=l_gb.vals, r_vals=r_gb.vals, 
+                                    n_intersect=intersect.shape[0], l_gr_idx=l_gb.gr_idx,
+                                    r_gr_idx=r_gb.gr_idx, l_idx=l_idx, r_idx=r_idx, 
+                                    null_val=null_val, how=how)
+        
--- merge/merge_functions.py
+++ merge/merge_functions.py
@@ -80,3 +80,68 @@ def _inner_merge(idx, l_vals, r_vals, n_intersect,
             res_gr[l_group_i[j]:l_group_i[j+1], l_end:] = r_arr
 
     return res
+
+
+@jit
+def _lr_merge_constr_res(idx, l_vals, r_vals, n_intersect, 
+                         l_gr_idx, r_gr_idx, l_idx, r_idx, 
+                         null_val, how=0):
+    """ Helper jitted function for inner merge """
+    # Make the results object of the proper proportions
+    # get counts per group
+    l_counts = np.diff(l_gr_idx)
+    r_counts_match = np.diff(r_gr_idx)[r_idx]
+    
+    r_counts = np.ones(l_counts.shape[0], np.int32)
+    r_counts[l_idx] = r_counts_match
+
+    # Multiplying these counts elementwise gives us the number of elements for each group
+    group_n = l_counts * r_counts
+    res_group_i = np.concatenate((np.array([0]), np.cumsum(group_n)))
+
+    n = res_group_i[-1] # number of total rows is the dot product of the each merge-group's lengths
+    m = idx.shape[1] + l_vals.shape[1] + r_vals.shape[1]
+
+    # Make an empty array to be filled in piece by piece
+    res = np.empty((n,m), dtype=idx.dtype)
+
+    # Generate the column-index start/stop combinations for (1) indices, (2) left values, and (3) right values
+    idx_end = idx.shape[1]
+    if how == 0: # left merge
+        l_begin, l_end = idx_end, idx_end + l_vals.shape[1]
+        r_begin, r_end = l_end, l_end + r_vals.shape[1]
+    elif how == 1: # right merge
+        r_begin, r_end = idx_end, idx_end + r_vals.shape[1]
+        l_begin, l_end = r_end, r_end + r_vals.shape[1]
+    
+    # We want to know when the group on the left has a match in the right
+    idx_isin_r = np.full(l_counts.shape[0], False)
+    idx_isin_r[l_idx] = True
+    # We also need to know where the right group matches start and stop
+    l2r_gr_idx = np.empty(l_gr_idx.shape[0], np.int32)
+    l2r_gr_idx[l_idx] = r_idx
+
+    # We iterate through each group in the left data object
+    for i in range(l_counts.shape[0]):
+
+        index = idx[l_gr_idx[i]] # a single row representation of the index columns (for left position)
+
+        l_arr = l_vals[l_gr_idx[i]:l_gr_idx[i+1]]
+        if idx_isin_r[i]:
+            # If the index-key at this iteration is shared, get the right-hand values
+            r_arr = r_vals[r_gr_idx[l2r_gr_idx[i]]:r_gr_idx[l2r_gr_idx[i]+1]]
+        else:
+            # If the index-key is only in the data being joined on, generate a null array
+            r_arr = np.full(r_vals.shape[1], null_val)
+
+        res_gr = res[res_group_i[i]:res_group_i[i+1]] # Get a view of the rows to be changed at this iteration
+
+        res_gr[:, :idx_end] = index
+
+        l_group_i = np.arange(l_counts[i]+1) * r_counts[i] # positions of each new row of l_arr in res_gr
+
+        for j in range(l_counts[i]):
+            res_gr[l_group_i[j]:l_group_i[j+1], l_begin:l_end] = l_arr[j]
+            res_gr[l_group_i[j]:l_group_i[j+1], r_begin:r_end] = r_arr
+
+    return res
